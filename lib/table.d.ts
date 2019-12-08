d7f2d7dbd780b3ae6676ba695c0f8229225661dc
bluelovers <codelovers@users.sourceforge.net>
2019-12-08 16:24:06 +0800

    postpublish

--- lib/table.d.ts
+++ lib/table.d.ts
@@ -4,9 +4,9 @@
 import { IDependency } from './cli/ncu';
 import Table = require('cli-table3');
 import { IStylesColorNames } from 'debug-color2/lib/styles';
-export declare function createDependencyTable(): Table.GenericTable<Table.Cell[] | Table.VerticalTableRow | Table.CrossTableRow>;
+export declare function createDependencyTable(): Table.Table;
 export declare function toDependencyTable(args: {
     from: IDependency;
     to: IDependency;
-}): Table.GenericTable<Table.Cell[] | Table.VerticalTableRow | Table.CrossTableRow>;
+}): Table.Table;
 export declare function colorizeDiff(from: string, to: string, _colors?: [IStylesColorNames, IStylesColorNames, IStylesColorNames], con?: import("debug-color2").Console2): string;
