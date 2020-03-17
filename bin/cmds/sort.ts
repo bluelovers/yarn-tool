1551f9bfdc3a2f6b6900823fbf235e00b413e49e
bluelovers <codelovers@users.sourceforge.net>
2020-03-17 11:00:41 +0800

    chore(release): publish

--- bin/cmds/sort.ts
+++ bin/cmds/sort.ts
@@ -2,7 +2,7 @@
  * Created by user on 2019/5/19.
  */
 import { basenameStrip, createCommandModuleExports } from '../../lib/cmd_dir';
-import path = require('upath2');
+import { join, relative } from 'upath2';
 import { chalkByConsole, consoleDebug, findRoot } from '../../lib/index';
 import { readPackageJson } from '@ts-type/package-dts';
 import { writePackageJson } from '../../lib/pkg';
@@ -32,20 +32,20 @@ const cmdModule = createCommandModuleExports({
 			cwd: argv.cwd,
 		}, true);
 
-		let json_file = path.join(rootData.pkg, 'package.json');
+		let json_file = join(rootData.pkg, 'package.json');
 
 		let json = readPackageJson(json_file);
 
 		// @ts-ignore
-		(require('sort-package-json') as typeof import('sort-package-json')).sortPackageJson(json);
+		json = (require('sort-package-json') as typeof import('sort-package-json')).sortPackageJson(json);
 
 		writePackageJson(json_file, json);
 
 		!argv.silent && chalkByConsole((chalk, console) => {
 
-			let p = chalk.cyan(path.relative(argv.cwd, json_file));
+			let p = chalk.cyan(relative(argv.cwd, json_file));
 
-			console.log(`${p} is sorted!`);
+			console.log(`[${chalk.cyan(json.name)}]`, `${p} is sorted!`);
 
 		}, consoleDebug);
 
